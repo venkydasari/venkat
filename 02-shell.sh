094423e3f2f06068c0e5ab5cd3178237823869ea
venky.dasari <venky.dasari@gmail.com>
2023-08-21 07:44:05 -0700

    script to check the package installation

--- 02-shell.sh
+++ 02-shell.sh
@@ -8,30 +8,30 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
-if [ $USERID -ne 0 ]
+if [$USERID -ne 0];
 then
-echo -e "$R error::Please install with admin access $N"
+echo -e "$R error::Please install with admin access $N";
 exit 1
 fi
 VALIDATE(){
-    if [$1 -ne 0]
+    if [$1 -ne 0];
     then
-    echo -e " Installing $2 ...$R FAILUR $N"
+    echo -e " Installing $2 ...$R FAILUR $N";
     else
-    echo -e " Installing $2 ...$G SUCCESS $N"
+    echo -e " Installing $2 ...$G SUCCESS $N";
     fi
 }
 for i in $@
 
 do
     yum list installed $i &>>$LOGFILE
-    if [ $? -ne 0 ]
+    if [ $? -ne 0 ];
     then
-        echo "$i is not installed, let's install it"
+        echo "$i is not installed, let's install it";
         yum install $i -y &>>$LOGFILE
         VALIDATE $? "$i"
     else
-        echo -e "$Y $i is already installed $N"
+        echo -e "$Y $i is already installed $N";
     fi
 
     #yum install $i -y
